1611aeef00eac8e704255330c18392d088240f04
Nicolas Acevedo <nicolasai24@educastur.es>
2024-03-18 13:25:24 +0100

    Añadimos los comentarios de documentacion en claseExamen

--- examenNicolas/src/main/java/miexamen/claseExamen.java
+++ examenNicolas/src/main/java/miexamen/claseExamen.java
@@ -1,14 +1,15 @@
+//Entornos de desarrolo UT3
 package miexamen;
-
+/**
+*Esta clase sirve para realizar el examen de la UT3 del modulo Entornos de Desarrolo
+*@see <a href = "https://www.youtube.com/playlist?list=PLU8oAlHdN5BlyaPFiNQcV0xDqy0eR35aU"/> Curso de Git y github de pildoras informaticas </a>
+* @author Nicolas
+* @version 1.0.0
+* @since 18/03/2024
+*/
 public class claseExamen {
 
     public static void main(String[] args) {
-        System.out.println("Estoy haciendo el examen de la UT3");
-        
-        System.out.println("Entornos de desarrollo UT3");
-        
-        System.out.println("Esta clase sirve para realizar el examen de la UT3 del modulo Entornos de Desarrolo");
-        
-        
+        System.out.println("Estoy haciendo el examen de la UT3");  
     }
 }
