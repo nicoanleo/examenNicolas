6de10ea2ce8cc275be6720a92fc273a26d9740cf
Nicolas Acevedo <nicolasai24@educastur.es>
2024-03-18 12:50:21 +0100

    Añadimos los primeros ficheros al proyecto

---
+++ examenNicolas/src/main/java/miexamen/claseExamen.java
@@ -0,0 +1,14 @@
+package miexamen;
+
+public class claseExamen {
+
+    public static void main(String[] args) {
+        System.out.println("Estoy haciendo el examen de la UT3");
+        
+        System.out.println("Entornos de desarrollo UT3");
+        
+        System.out.println("Esta clase sirve para realizar el examen de la UT3 del modulo Entornos de Desarrolo");
+        
+        
+    }
+}
